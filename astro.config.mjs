a6f23cc61ef3a9862ed24019629311d7fa0367b1
Bryson Meiling <bryson.meiling@gmail.com>
2025-12-28 23:48:08 -0700

    added scaffold for the pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bluelogicwater.com",
+  output: "server",
   adapter: cloudflare(),
   integrations: [sitemap()],
   vite: {
