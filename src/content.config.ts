1ab696df9227e078866cb834b343b10d3332088b
Bryson Meiling <bryson.meiling@gmail.com>
2026-01-05 14:11:03 -0800

    making blog image work

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,17 +5,18 @@ import { z } from "astro/zod";
 // 4. Define your collection(s)
 const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
-  schema: z.object({
-    slug: z.string().regex(/^[a-zA-Z0-9\-\_]+$/, {
-      message:
-        "Slug can only contain letters, numbers, hyphens, and underscores",
+  schema: ({ image }) =>
+    z.object({
+      slug: z.string().regex(/^[a-zA-Z0-9\-\_]+$/, {
+        message:
+          "Slug can only contain letters, numbers, hyphens, and underscores",
+      }),
+      title: z.string(),
+      description: z.string().optional(),
+      author: z.string(),
+      date: z.coerce.date(),
+      image: image().optional(),
     }),
-    title: z.string(),
-    description: z.string().optional(),
-    author: z.string(),
-    date: z.coerce.date(),
-    image: z.string().optional(),
-  }),
 });
 
 // 5. Export a single `collections` object to register your collection(s)
